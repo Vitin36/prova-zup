466b5c806a3b14c0509452127184e6f73adea71c
Vitin36 <vitin_36@hotmail.com>
2019-01-28 05:00:11 -0200

    funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', redirectTo: 'listagem', pathMatch: 'full' },
+  { path: 'listagem', loadChildren: './com-menu/com-menu.module#ComMenuModule' },
+  { path: 'usuario', loadChildren: './sem-menu/sem-menu.module#SemMenuModule' },
+  { path : "**", redirectTo : 'listagem' , pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RouterService } from './services/router.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'zup-vitor-hugo';
+
+  constructor(private routerService : RouterService) { }
+
+  ngOnInit() { }
+
 }
